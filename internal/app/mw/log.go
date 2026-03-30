78bd22a6c54bae473e2d604b10049fd5cafffa8d
Sergey Lazarenko <serlazarenko@ozon.ru>
2026-03-30 13:53:54 +0300

    remove query in logs

--- internal/app/mw/log.go
+++ internal/app/mw/log.go
@@ -2,7 +2,6 @@ package mw
 
 import (
 	"context"
-	"encoding/json"
 	"strings"
 	"time"
 
@@ -72,14 +71,6 @@ func logRequestBeforeHandler(ctx context.Context, logger *tracing.Logger, args r
 		zap.Object("header", processedHeader),
 		zap.String("full_method", args.fullMethod),
 		zap.String("body", args.requestBody),
-	}
-	if args.requestBody != "" {
-		var requestData map[string]any
-		if err := json.Unmarshal([]byte(args.requestBody), &requestData); err == nil {
-			if q, ok := requestData["query"].(string); ok && q != "" {
-				logArgs = append(logArgs, zap.String("query", q))
-			}
-		}
 	}
 	if args.user != "" {
 		logArgs = append(logArgs, zap.String("user", args.user))
