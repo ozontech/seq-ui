2575bce8da0abfb2fa299ebbe7380ea760a230cc
Sergey Lazarenko <serlazarenko@ozon.ru>
2026-04-17 10:16:12 +0300

    bib

--- internal/api/admin/v1/http/api.go
+++ internal/api/admin/v1/http/api.go
@@ -18,17 +18,5 @@ func New(svc service.Service) *API {
 func (a *API) Router() chi.Router {
 	mux := chi.NewMux()
 
-	mux.Route("/roles", func(r chi.Router) {
-		r.Post("/", a.serveCreateRole)
-		r.Get("/", a.serveGetRoles)
-
-		r.Route("/{id}", func(r chi.Router) {
-			r.Post("/users", a.serveAddUsersToRole)
-			r.Get("/", a.serveGetRole)
-			r.Patch("/", a.serveUpdateRole)
-			r.Delete("/", a.serveDeleteRole)
-		})
-	})
-
 	return mux
 }
