777789014235eaae7464c36f4300fbfc3cdcf368
Sergey Lazarenko <serlazarenko@ozon.ru>
2026-04-17 14:23:50 +0300

    fix

--- migration/12_admin_initial.sql
+++ migration/12_admin_initial.sql
@@ -5,9 +5,13 @@ CREATE TABLE IF NOT EXISTS roles(
     name TEXT NOT NULL UNIQUE,
     permissions BIGINT NOT NULL DEFAULT 0
 );
+
+ALTER TABLE IF EXISTS user_profiles ADD COLUMN IF NOT EXISTS role_id INTEGER;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE IF EXISTS user_profiles DROP COLUMN IF EXISTS role_id;
+
 DROP TABLE IF EXISTS roles;
 -- +goose StatementEnd
--- migration/13_after_table_roles_add_column_role_id.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE IF EXISTS user_profiles ADD COLUMN IF NOT EXISTS role_id INTEGER;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE IF EXISTS user_profiles DROP COLUMN IF EXISTS role_id;
--- +goose StatementEnd
