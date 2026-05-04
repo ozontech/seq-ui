7d1e469f64c665174067bc183e343f5b142794f0
Sergey Lazarenko <serlazarenko@ozon.ru>
2026-05-04 13:17:12 +0300

    grpc admin

--- internal/api/admin/v1/grpc/roles.go
+++ internal/api/admin/v1/grpc/roles.go
@@ -7,11 +7,23 @@ import (
 	"github.com/ozontech/seq-ui/internal/app/types"
 	"github.com/ozontech/seq-ui/pkg/admin/v1"
 	"github.com/ozontech/seq-ui/tracing"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 func (a *API) CreateRole(ctx context.Context, req *admin.CreateRoleRequest) (*admin.CreateRoleResponse, error) {
 	ctx, span := tracing.StartSpan(ctx, "admin_v1_create_role")
 	defer span.End()
+
+	span.SetAttributes(
+		attribute.KeyValue{
+			Key:   "role_name",
+			Value: attribute.StringValue(req.GetName()),
+		},
+		attribute.KeyValue{
+			Key:   "permissions_count",
+			Value: attribute.IntValue(len(req.GetPermissions())),
+		},
+	)
 
 	request := types.CreateRoleRequest{
 		Name:        req.Name,
@@ -29,21 +41,146 @@ func (a *API) CreateRole(ctx context.Context, req *admin.CreateRoleRequest) (*ad
 }
 
 func (a *API) AddUsersToRole(ctx context.Context, req *admin.AddUsersToRoleRequest) (*admin.AddUsersToRoleResponse, error) {
+	ctx, span := tracing.StartSpan(ctx, "admin_v1_add_users_to_role")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.KeyValue{
+			Key:   "role_id",
+			Value: attribute.IntValue(int(req.GetRoleId())),
+		},
+		attribute.KeyValue{
+			Key:   "users_count",
+			Value: attribute.IntValue(len(req.GetUsernames())),
+		},
+	)
+
+	if err := a.service.AddUsersToRole(ctx, types.AddUsersToRoleRequest{
+		RoleID:    req.RoleId,
+		Usernames: req.Usernames,
+	}); err != nil {
+		return nil, grpcutil.ProcessError(err)
+	}
+
 	return &admin.AddUsersToRoleResponse{}, nil
 }
 
-func (a *API) GetRoles(ctx context.Context, req *admin.GetRolesRequest) (*admin.GetRolesResponse, error) {
-	return &admin.GetRolesResponse{}, nil
+func (a *API) GetRoles(ctx context.Context, _ *admin.GetRolesRequest) (*admin.GetRolesResponse, error) {
+	resp, err := a.service.GetRoles(ctx)
+	if err != nil {
+		return nil, grpcutil.ProcessError(err)
+	}
+
+	return &admin.GetRolesResponse{
+		Roles:                rolesToProto(resp.Roles),
+		AvailablePermissions: availablePermissionsToProto(resp.AvailablePermissions),
+	}, nil
 }
 
 func (a *API) GetRole(ctx context.Context, req *admin.GetRoleRequest) (*admin.GetRoleResponse, error) {
-	return &admin.GetRoleResponse{}, nil
+	ctx, span := tracing.StartSpan(ctx, "admin_v1_get_role")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.KeyValue{
+			Key:   "role_id",
+			Value: attribute.IntValue(int(req.GetId())),
+		},
+	)
+
+	resp, err := a.service.GetRole(ctx, types.GetRoleRequest{
+		RoleID: req.Id,
+	})
+	if err != nil {
+		return nil, grpcutil.ProcessError(err)
+	}
+
+	return &admin.GetRoleResponse{
+		Usernames: resp.Usernames,
+	}, nil
 }
 
 func (a *API) UpdateRole(ctx context.Context, req *admin.UpdateRoleRequest) (*admin.UpdateRoleResponse, error) {
+	ctx, span := tracing.StartSpan(ctx, "admin_v1_update_role")
+	defer span.End()
+
+	spanAttributes := []attribute.KeyValue{
+		{
+			Key:   "role_id",
+			Value: attribute.IntValue(int(req.GetId())),
+		},
+		{
+			Key:   "permissions_count",
+			Value: attribute.IntValue(len(req.GetPermissions())),
+		},
+	}
+	if req.Name != nil {
+		spanAttributes = append(spanAttributes, attribute.KeyValue{
+			Key:   "role_name",
+			Value: attribute.StringValue(req.GetName()),
+		})
+	}
+	span.SetAttributes(spanAttributes...)
+
+	if err := a.service.UpdateRole(ctx, types.UpdateRoleRequest{
+		RoleID:      req.Id,
+		Name:        req.Name,
+		Permissions: req.Permissions,
+	}); err != nil {
+		return nil, grpcutil.ProcessError(err)
+	}
+
 	return &admin.UpdateRoleResponse{}, nil
 }
 
 func (a *API) DeleteRole(ctx context.Context, req *admin.DeleteRoleRequest) (*admin.DeleteRoleResponse, error) {
+	ctx, span := tracing.StartSpan(ctx, "admin_v1_delete_role")
+	defer span.End()
+
+	spanAttributes := []attribute.KeyValue{
+		{
+			Key:   "role_id",
+			Value: attribute.IntValue(int(req.GetId())),
+		},
+	}
+	if req.ReplacementRoleId != nil {
+		spanAttributes = append(spanAttributes, attribute.KeyValue{
+			Key:   "replacement_role_id",
+			Value: attribute.IntValue(int(req.GetReplacementRoleId())),
+		})
+	}
+	span.SetAttributes(spanAttributes...)
+
+	if err := a.service.DeleteRole(ctx, types.DeleteRoleRequest{
+		RoleID:            req.Id,
+		ReplacementRoleID: req.ReplacementRoleId,
+	}); err != nil {
+		return nil, grpcutil.ProcessError(err)
+	}
+
 	return &admin.DeleteRoleResponse{}, nil
+}
+
+func rolesToProto(source []types.Role) []*admin.Role {
+	roles := make([]*admin.Role, 0, len(source))
+	for _, role := range source {
+		roles = append(roles, &admin.Role{
+			Id:          role.ID,
+			Name:        role.Name,
+			Permissions: role.Permissions,
+		})
+	}
+	return roles
+}
+
+func availablePermissionsToProto(source []types.Permission) []*admin.GetRolesResponse_Permission {
+	availablePermissions := make([]*admin.GetRolesResponse_Permission, 0, len(source))
+	for _, aPermission := range source {
+		availablePermissions = append(availablePermissions, &admin.GetRolesResponse_Permission{
+			Value:       aPermission.Value,
+			Name:        aPermission.Name,
+			Description: aPermission.Description,
+		})
+	}
+	return availablePermissions
 }
